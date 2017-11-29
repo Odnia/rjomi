18c38a492deaf5c3b608cda6ad6e9b03723b2f5b
Odnia <34063740+Odnia@users.noreply.github.com>
2017-11-29 19:00:56 +0000

    Update CashierUI.cpp

--- main_assignment/src/CashierUI.cpp
+++ main_assignment/src/CashierUI.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
 #include "CashierUI.h"
+#include "MainUI.h"
 
 CashierUI::CashierUI()
 {
@@ -14,15 +19,22 @@ void CashierUI::startUI()
 {
     char choice = '\0';
     while(choice != 'b') {
-    system("CLS");
-    int choice;
-    cout << "Cashier: " << endl;
-    cout << " +------------------+" << endl;
-    cout << " |1. Your location  |" << endl;
-    cout << " |2. Back           |" << endl;
-    cout << " |3. Quit           |" << endl;
-    cout << " +------------------+" << endl;
-    cout << " (1-3): ";
-    cin >> choice;
+        system("CLS");
+        cout << "Cashier: " << endl;
+        cout << " +------------------+" << endl;
+        cout << " |1. Your location  |" << endl;
+        cout << " |2. Charge Order   |" << endl;
+        cout << " |3. Back           |" << endl;
+        cout << " +------------------+" << endl;
+        cout << " (1-3): ";
+        cin >> choice;
+        
+        if (choice == '1') {
+            //Location location;
+        } else if (choice == '2') { 
+            //
+        } else if (choice == '3') {
+            return;
+        }
     }
 }
